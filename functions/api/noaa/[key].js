3acb27003f3f3aa5e93ab5c51f835cc34451083a
Santeri Kääriäinen <santeri.kaariainen@iki.fi>
2022-03-04 16:31:59 +0200

    debug2

--- functions/api/noaa/[key].js
+++ functions/api/noaa/[key].js
@@ -1,12 +1,6 @@
-const onRequestGet = async ({env, params}) => {
+export const onRequestGet = async ({env, params}) => {
   const url = new URL("https://" + env.CF_DOMAIN)
   url.host = env.CF_DOMAIN
   url.pathname = params.key
   return fetch(url.toString())
 }
-/*
-onRequestGet({
-  params: { key: "test.jpeg"},
-  env: { CF_DOMAIN: "kissa.cloudfront.net" }
-})
-*/
